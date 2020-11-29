bb64941b67b6b04c6e70fd7e749f5007b3257f64
ericcornetto <putrapurnawa@gmail.com>
2020-11-29 12:21:34 +0800

    update booking

--- src/booking/Booking.js
+++ src/booking/Booking.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import BookingList from './BookingList';
-import BookingDetails from './BookingDetails';
 
 const Booking = () => {
     
@@ -10,7 +9,6 @@ const Booking = () => {
     return(
         <Stack.Navigator>
             <Stack.Screen name="Booking List" component={BookingList} options={settings} />
-            <Stack.Screen name="Booking Details" component={BookingDetails} options={detailsSetting} />
         </Stack.Navigator>
     );
 
@@ -26,17 +24,6 @@ const settings = {
     },
     title: "Booking",
     headerLeft: null,
-}
-
-const detailsSetting = {
-    headerStyle: {
-        backgroundColor: "#3588E7",
-    },
-    headerTintColor: "#FFFFFF",
-    headerTitleStyle: {
-        fontWeight: 'bold'
-    },
-    title: "Booking Details",
 }
 
 export default Booking;
